7150383813524372da753973a79d6b171dea20fb
unknown <sondess.landolsi@laposte.net>
2022-04-18 13:43:14 +0200

    modifications server/app

--- backend/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const userCtrl = require('../controllers/user');
-const auth = require('../middleware/auth')
-
-
-module.exports = router;
